1a218ba4ba2aa5642981f2bfe407d295c354e6cd
Savya Sarda <86636104+Savya-Sarda@users.noreply.github.com>
2022-02-19 10:43:49 +0800

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,21 +1,14 @@
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBz0wA59tizKLrHYEGtnX0TunBgNhdUKME",
-    authDomain: "kwitter-app-2bc22.firebaseapp.com",
-    databaseURL: "https://kwitter-app-2bc22-default-rtdb.firebaseio.com",
-    projectId: "kwitter-app-2bc22",
-    storageBucket: "kwitter-app-2bc22.appspot.com",
-    messagingSenderId: "885504915722",
-    appId: "1:885504915722:web:5b41ca711061d5468062f3"
+    apiKey: "AIzaSyAdRSakrx3Gc-QZ4Zs9oRZwS5cdvN9F6tc",
+    authDomain: "kwitter-55.firebaseapp.com",
+    databaseURL: "https://kwitter-55-default-rtdb.firebaseio.com",
+    projectId: "kwitter-55",
+    storageBucket: "kwitter-55.appspot.com",
+    messagingSenderId: "886304295938",
+    appId: "1:886304295938:web:fc6b5fc6226f624d76ab1b"
   };
-  function logout(){
-      
-    localStorage.removeItem("user_name");
-    localStorage.removeItem("room_name");
-          window.location = "index.html";
-
-
-}
+  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
@@ -33,7 +26,14 @@ var firebaseConfig = {
 
       document.getElementById("msg").value = "";
   }
+  function logout(){
+      
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+          window.location = "index.html";
 
+
+}
   function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
     firebase_message_id = childKey;
     message_data = childData;
--- kwitter_room.js
+++ kwitter_room.js
@@ -1,13 +1,13 @@
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-      apiKey: "AIzaSyBz0wA59tizKLrHYEGtnX0TunBgNhdUKME",
-      authDomain: "kwitter-app-2bc22.firebaseapp.com",
-      databaseURL: "https://kwitter-app-2bc22-default-rtdb.firebaseio.com",
-      projectId: "kwitter-app-2bc22",
-      storageBucket: "kwitter-app-2bc22.appspot.com",
-      messagingSenderId: "885504915722",
-      appId: "1:885504915722:web:5b41ca711061d5468062f3"
+      apiKey: "AIzaSyAdRSakrx3Gc-QZ4Zs9oRZwS5cdvN9F6tc",
+      authDomain: "kwitter-55.firebaseapp.com",
+      databaseURL: "https://kwitter-55-default-rtdb.firebaseio.com",
+      projectId: "kwitter-55",
+      storageBucket: "kwitter-55.appspot.com",
+      messagingSenderId: "886304295938",
+      appId: "1:886304295938:web:fc6b5fc6226f624d76ab1b"
     };
     
     // Initialize Firebase
